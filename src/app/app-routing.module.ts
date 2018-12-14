6a086dff8e078ff282783755131bfa0999225e9a
vamseeuxd <vamsee.uxd@gmail.com>
2018-12-14 09:57:59 +0530

    Refactored Order System Page Folder structure.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-    {path: 'login', loadChildren: './tabs/tabs.module#TabsPageModule'},
-    {path: 'profile', loadChildren: './public/profile/profile.module#ProfilePageModule'},
-    {path: '', loadChildren: './public/login/login.module#LoginPageModule'}
+    {path: '', loadChildren: './pages/order-system/tabs/tabs.module#TabsPageModule'},
+    {path: 'profile', loadChildren: './pages/public/profile/profile.module#ProfilePageModule'},
+    {path: 'login', loadChildren: './pages/public/login/login.module#LoginPageModule'}
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
