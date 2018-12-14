cac026facd73f3b20d6710ba6fbf7ac8d41a0464
vamseeuxd <vamsee.uxd@gmail.com>
2018-12-14 09:41:16 +0530

    Refactored Login and Profile Pages Folder structure.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
     {path: 'login', loadChildren: './tabs/tabs.module#TabsPageModule'},
-    {path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule'},
-    {path: '', loadChildren: './login/login.module#LoginPageModule'}
+    {path: 'profile', loadChildren: './public/profile/profile.module#ProfilePageModule'},
+    {path: '', loadChildren: './public/login/login.module#LoginPageModule'}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
